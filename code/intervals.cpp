6f5439349915d664d10d0bd208589554cca0ddb9
student <my@yandex.ru>
2021-04-20 09:54:46 +0300

    Added interval example

--- code/intervals.cpp
+++ code/intervals.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
+#include <iomanip>
 #include <limits.h>
 
-#define inf (-INT_MAX)
+using namespace std;
 
-int op(int a, int b)
+#define inf (-1) //(-INT_MAX)
+
+int f_max(int a, int b)
 {
     return a>b?a:b;
 }
@@ -13,13 +16,16 @@ struct tree
     int *body;
     int size;
     int numnodes;
+
     tree(int maxsize);
     ~tree();
+    
     void swap(int a, int b);
     void print();
-    void propagate(int index);
-    void insert(int index, int value);
-    int func(int left, int right);
+
+    void propagate(int index, int (*f)(int, int));
+    void insert(int index, int value, int (*f)(int, int));    
+    int calculate(int left, int right, int (*f)(int, int));
 };
 
 tree::tree(int maxsize)
@@ -39,43 +45,45 @@ tree::~tree()
 
 void tree::swap(int a, int b)
 {
-    std::swap(body[a], body[b]);
+    swap(body[a], body[b]);
 }
 
 void tree::print()
 {    
     for (int i=0; i<size; i++)
-        printf("|%2d", i);
-    printf("|\n");
+        cout << "|" << setw(2) << i;
+    cout << "|" << endl;            
     for (int i=0; i<size; i++)
-        printf("|%2d", body[i]);
-    printf("|\n");
+        cout << "|" << setw(2) << body[i];    
+    cout << "|" << endl;
 }
 
-void tree::propagate(int index)
+void tree::propagate(int index, int (*f)(int, int))
 {
     while (index > 0)
     {
-        body[index/2] = op(body[index], body[index+1]);
+        body[index/2] = f(body[index], body[index/2]);
         index /= 2;
     }
 }
 
-void tree::insert(int index, int value)
+void tree::insert(int index, int value, int (*f)(int, int))
 {
     body[size/2 + index] = value;
-    propagate(size/2 + index);
+    numnodes++;
+
+    propagate(size/2 + index, f);
 }
 
-int tree::func(int left, int right)
+int tree::calculate(int left, int right, int (*f)(int, int))
 {
     int res = inf;
     if (left % 2 == 1)
-        res = op(res, body[left++]);
+        res = f(res, body[left++]);
     if (right % 2 == 0)
-        res = op(res, body[right--]);        
+        res = f(res, body[right--]);        
     if (right > left)
-        res = op(res, func(left/2, right/2));                
+        res = f(res, calculate(left/2, right/2, f));                
     return res;
 }
 
@@ -83,25 +91,35 @@ int main(void)
 {
     int size;
 
-    std::cin >> size;
+    cin >> size;
     
     tree t = tree(size);
-    for (int i=0; i<=t.size; i++) t.body[i] = inf;
+
+    for (int i=0; i<=t.size; i++) 
+        t.body[i] = inf;
+    
     for (int i=0; i<size; i++)
     {
         int value;
-        std::cin >> value;
-        t.insert(i, value);
+        
+        cin >> value;
+        
+        t.insert(i, value, f_max);
+
+        // cout << "=" << i << endl;
+        // t.print();
     }
 
-    int left, right;
+    int left, right, start = size;
 
-    std::cin >> left;
-    std::cin >> right;
+    cin >> left;
+    cin >> right;
 
     t.print();
 
-    printf("\nmax(%d, %d)=%d", left, right, t.func(left, right));
+    cout << "size=" << t.size << endl;
+    cout << "max(" << left << "," << right << ")=" 
+         << t.calculate(left+start, right+start, f_max);
 
     return 0;
 }
