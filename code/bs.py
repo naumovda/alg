b818eb24f5b93e32225bb9a64ab10eebe3a1849b
Naumov Dmitry <lonlymage@mail.ru>
2022-03-24 11:43:07 +0300

    Added binary search example

---
+++ code/bs.py
@@ -0,0 +1,28 @@
+bunch = [1, 2, 2, 4, 4, 4, 5, 6, 7, 8, 9]
+
+def lbs(bunch, item, left, right):
+    while right - left > 1:
+        middle = (right + left) // 2
+        if bunch[middle] < item:
+            left = middle
+        else:
+            right = middle
+    return left
+
+def rbs(bunch, item, left, right):
+    while right - left > 1:
+        middle = (right + left) // 2
+        if bunch[middle] <= item:
+            left = middle
+        else:
+            right = middle
+    return right
+
+def main():
+    print("bunch=", bunch)
+    key = 11
+    print(lbs(bunch, key, -1, len(bunch)))
+    print(rbs(bunch, key, -1, len(bunch)))
+
+if __name__ == "__main__":
+    main()
